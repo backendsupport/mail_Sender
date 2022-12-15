8d93ddd02cb0d884c875cbbef3ec93a765e81051
dhruv patel <dhruv160916@gmail.com>
2022-12-15 21:10:23 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from email.message import  EmailMessage
+import ssl
+import smtplib
+import json
+from flask import Flask, request, jsonify
+email_password='qksouevymaduszon'
+email_sender='varx4coder@gmail.com'
+app=Flask(__name__)
+
+
+@app.route('/mail', methods=['POST'])
+def send_mail():
+    record = json.loads(request.data)
+    email=record['email']
+    subject=record['subject']
+    body=record['message']
+    em=EmailMessage()
+    em['From']=email_sender
+    em['To']=email
+    em['Subject']=subject
+    em.set_content(body)
+
+    context=ssl.create_default_context()
+    with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as smtp:
+        smtp.login(email_sender,email_password)
+        smtp.sendmail(email_sender,email,em.as_string())  
+  
+    return 'succces'
+if __name__=="__main__":
+    app.run(debug=True)
